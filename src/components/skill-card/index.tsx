4ecfab3083e500b6a041e3449042aa12bf20aa28
srijan mukherjee <emailofsrijan@gmail.com>
2023-12-22 02:52:42 +0530

    added skill card

---
+++ src/components/skill-card/index.tsx
@@ -0,0 +1,23 @@
+import { Flex, MantineColor, ThemeIcon, rem } from "@mantine/core";
+import { StaticImageData } from "next/image";
+import Image from "next/image";
+
+import classes from "./skillcard.module.css";
+
+export interface SkillCardProps {
+    src: string | StaticImageData;
+    alt: string;
+    value: string;
+    color?: MantineColor;
+}
+
+export default function SkillCard({ src, value, color = "white", alt }: SkillCardProps) {
+    return (
+        <Flex className={classes.container}>
+            <ThemeIcon size="xl" radius="md" color={color} variant="light">
+                <Image src={src} alt={alt} width={28} placeholder="blur" />
+            </ThemeIcon>
+            {value}
+        </Flex>
+    );
+}
