145385b66351db15cbfd64bfcbd94513ec9c94c3
srijan mukherjee <emailofsrijan@gmail.com>
2023-12-22 02:52:52 +0530

    added skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import {
     Flex,
     Group,
     List,
-    ListItem,
     Stack,
     Text,
     ThemeIcon,
@@ -23,28 +22,32 @@ import { Typewriter } from "react-simple-typewriter";
 import classes from "./page.module.css";
 
 import myself from "@/assets/img/me.jpg";
+import cIcon from "@/assets/img/languages/c.png";
+import cppIcon from "@/assets/img/languages/cpp.png";
+import javaIcon from "@/assets/img/languages/java.png";
+import pythonIcon from "@/assets/img/languages/python.png";
+import javascriptIcon from "@/assets/img/languages/javascript.png";
+import typescriptIcon from "@/assets/img/languages/typescript.png";
+import nextjsIcon from "@/assets/img/technologies/nextjs.png";
+import reactIcon from "@/assets/img/technologies/react.png";
+import nodejsIcon from "@/assets/img/technologies/nodejs.png";
+import springIcon from "@/assets/img/technologies/spring.png";
+import postgresqlIcon from "@/assets/img/technologies/postgresql.png";
+import mongodbIcon from "@/assets/img/technologies/mongodb.png";
 import links from "@/data/links.json";
 import {
-    IconArrowAutofitContent,
     IconArrowRight,
-    IconArrowRightCircle,
     IconBabyBottle,
     IconBadge,
     IconBook,
     IconBrandUpwork,
     IconDeviceLaptop,
     IconFlower,
-    IconFocus,
     IconFocus2,
-    IconGitBranch,
-    IconGitCommit,
-    IconGitPullRequest,
-    IconHomeDot,
-    IconMessageDots,
     IconSchool,
-    IconSchoolOff,
     IconTree,
 } from "@tabler/icons-react";
+import SkillCard, { SkillCardProps } from "@/components/skill-card";
 
 const typewritterIntros = [
     "Software Developer",
@@ -52,6 +55,94 @@ const typewritterIntros = [
     "I am also a freelancer",
     "Check out my works below",
 ];
+
+const languages: SkillCardProps[] = [
+    {
+        value: "C",
+        color: "blue",
+        src: cIcon,
+        alt: "C Programming Language",
+    },
+    {
+        value: "C++",
+        color: "blue",
+        src: cppIcon,
+        alt: "C++ Programming Language",
+    },
+    {
+        value: "Java",
+        color: "white",
+        src: javaIcon,
+        alt: "Java Programming Language",
+    },
+    {
+        value: "Python",
+        color: "blue",
+        src: pythonIcon,
+        alt: "Python Programming Language",
+    },
+    {
+        value: "Javascript",
+        color: "yellow",
+        src: javascriptIcon,
+        alt: "Javascript Programming Language",
+    },
+    {
+        value: "Typescript",
+        color: "blue",
+        src: typescriptIcon,
+        alt: "Typescript Programming Language",
+    },
+];
+
+const technologies: SkillCardProps[] = [
+    {
+        value: "NextJS",
+        color: "#fff",
+        src: nextjsIcon,
+        alt: "Nextjs",
+    },
+    {
+        value: "React",
+        color: "blue",
+        src: reactIcon,
+        alt: "React",
+    },
+    {
+        value: "NodeJS",
+        color: "green",
+        src: nodejsIcon,
+        alt: "Node.JS",
+    },
+    {
+        value: "Spring Framework",
+        color: "blue",
+        src: springIcon,
+        alt: "Spring",
+    },
+    {
+        value: "Spring Boot",
+        color: "blue",
+        src: springIcon,
+        alt: "Spring Boot",
+    },
+];
+
+const databases: SkillCardProps[] = [
+    {
+        value: "PostgreSQL",
+        color: "blue",
+        src: postgresqlIcon,
+        alt: "Nextjs",
+    },
+    {
+        value: "MongoDB",
+        color: "green",
+        src: mongodbIcon,
+        alt: "MongoDB",
+    },
+];
+
 export default function Home() {
     return (
         <Container pb={20} size={containerSize}>
@@ -198,7 +289,41 @@ export default function Home() {
                         style={{ alignItems: "center", gap: "10px" }}>
                         <IconBook size={32} /> Skills
                     </Title>
-                    <Alert color="red">Construction is going on</Alert>
+
+                    <Stack gap="xl">
+                        <Stack gap="xs">
+                            <Title order={3} c="dimmed">
+                                Languages
+                            </Title>
+                            <Group>
+                                {languages.map((props) => (
+                                    <SkillCard {...props} key={props.value} />
+                                ))}
+                            </Group>
+                        </Stack>
+
+                        <Stack gap="xs">
+                            <Title order={3} c="dimmed">
+                                Technologies
+                            </Title>
+                            <Group>
+                                {technologies.map((props) => (
+                                    <SkillCard {...props} key={props.value} />
+                                ))}
+                            </Group>
+                        </Stack>
+
+                        <Stack gap="xs">
+                            <Title order={3} c="dimmed">
+                                Databases
+                            </Title>
+                            <Group>
+                                {databases.map((props) => (
+                                    <SkillCard {...props} key={props.value} />
+                                ))}
+                            </Group>
+                        </Stack>
+                    </Stack>
                 </Box>
             </Stack>
         </Container>
