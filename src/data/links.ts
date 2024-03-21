4f2ed6b213533ff98a524bbee0fbb5960aa58780
Srijan Mukherjee <emailofsrijan@gmail.com>
2024-03-22 00:55:07 +0530

    data: updated resume

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,5 +1,5 @@
 export const resume =
-    "https://drive.google.com/file/d/1JP92al_c-Vszjz0jdD8eKMUm-V9S-TN9/view?usp=sharing";
+    "https://drive.google.com/file/d/1vDqJaHle4oQbTlT0zd06O64LvytLLlWk/view?usp=sharing";
 
 export const github = "https://www.github.com/srijanmukherjee";
 
